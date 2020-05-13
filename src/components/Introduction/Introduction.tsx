dd4198b5f489b00cdc7d3b0e5518bc908bcca276
Vainius <Vainius@PC737>
2020-05-13 17:26:45 +0300

    fixed buttons and added responsive css

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -4,7 +4,7 @@ import './Introduction.css';
 const Introduction = () => {
     return(
         <div className="background">
-          <div className="introduction">
+          <div className="introduction" id="introduction">
             <h1>Vainius Baranauskas</h1>
             <h2>web developer</h2>
           </div>
--- src/components/SideDrawer/SideDrawer.tsx
+++ src/components/SideDrawer/SideDrawer.tsx
@@ -20,53 +20,113 @@ const SideDrawer = (props: IProps) => {
     
     return (
         <nav className={drawerClasses}>
-            <ul>
+            <div className="buttons">
                 <Link
-                 activeClass="active"
-                 to="Education"
-                 onClick={props.click}
-                 spy={true}
-                 smooth={true}
-                 offset={0}
-                 duration={1500}
-                >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    Education
+                    activeClass="active"
+                    to="introduction"
+                    onClick={props.click}
+                    spy={true}
+                    smooth={true}
+                    offset={-400}
+                    duration={1500}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Name
                 </Link>
                 <Link
-                 activeClass="active"
-                 to="Experience"
-                 onClick={props.click}
-                 spy={true}
-                 smooth={true}
-                 offset={0}
-                 duration={1500}
-                >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    Experience
+                    activeClass="active"
+                    to="TechStack"
+                    onClick={props.click}
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={1500}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        About me
                 </Link>
                 <Link
-                 activeClass="active"
-                 to="TechStack"
-                 onClick={props.click}
-                 spy={true}
-                 smooth={true}
-                 offset={0}
-                 duration={1500}
-                >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    Tech stack
+                    activeClass="active"
+                    to="TechStack"
+                    onClick={props.click}
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={1500}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Tech stack
                 </Link>
-            </ul>
+                <Link
+                    activeClass="active"
+                    to="Experience"
+                    onClick={props.click}
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={1500}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Experience
+                </Link>
+                <Link
+                    activeClass="active"
+                    to="Education"
+                    onClick={props.click}
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={1500}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Education
+                </Link>
+                <Link
+                    activeClass="active"
+                    to="Education"
+                    onClick={props.click}
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={1500}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Personal projects 
+                </Link>
+                <Link
+                    activeClass="active"
+                    to="Education"
+                    onClick={props.click}
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={1500}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Contact me 
+                </Link>
+            </div>
         </nav>
     );
 };
